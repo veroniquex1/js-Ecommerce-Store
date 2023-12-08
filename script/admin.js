a6cb2eaa7a8d7593d0ec8b84b94fccfbf8b470e0
veroniquex1 <vdrossouw@gmail.com>
2023-12-08 17:09:11 +0200

    final changes

--- script/admin.js
+++ script/admin.js
@@ -52,3 +52,53 @@ function deleteItem(){
 let table = document.querySelector('[data-table-body]')
 
 table.addEventListener('click',deleteItem)
+
+/*
+function editProducts(index) {
+  let editName = document.querySelector("[data-edit-name]");
+  let editMake = document.querySelector("[data-edit-make]");
+  let editDescription = document.querySelector("[data-edit-description]");
+  let editPrice = document.querySelector("[data-edit-price]");
+  let editImg = document.querySelector("[data-edit-img]");
+  editName.value = products[index].name;
+  editMake.value = products[index].make;
+  editDescription.value = products[index].description;
+  editPrice.value = products[index].price;
+  editImg.value = products[index].img;
+  // Store the index in a data attribute for later use
+  document.querySelector("#productEdit").setAttribute("data-edit-index", index);
+}
+// Add this function to save changes when the "Save changes" button is clicked
+function saveChanges() {
+  // Retrieve values from inputs
+  let editedName = document.querySelector("[data-edit-name]").value;
+  let editedMake = document.querySelector("[data-edit-make]").value;
+  let editedDescription = document.querySelector(
+    "[data-edit-description]"
+  ).value;
+  let editedPrice = document.querySelector("[data-edit-price]").value;
+  let editedImg = document.querySelector("[data-edit-img]").value;
+  // Retrieve the index from the data attribute
+  // let index = document.querySelector('#productEdit').getAttribute('data-edit-index');
+  let index = document.querySelector("[data-edit]").value;
+  // Update data in your array
+  products[index].name = editedName;
+  products[index].make = editedMake;
+  products[index].description = editedDescription;
+  products[index].price = editedPrice;
+  products[index].img = editedImg;
+  // Update the localStorage
+  update();
+}
+productDisplay.addEventListener("click", function (event) {
+  if (event.target.classList.contains("edit")) {
+    editProducts(event.target.value);
+  }
+  update();
+});
+document.querySelector("[data-saveEdited-prod]").addEventListener(
+  "click",
+  saveChanges,
+  update,
+  display
+); */
